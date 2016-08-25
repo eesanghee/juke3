c3a664b27ffc81ae9ad3e892ae0a23739cff4d94
Ran Duan <rnduan@gmail.com>
2016-08-25 15:51:23 -0400

    Next steps

--- browser/js/module.js
+++ browser/js/module.js
@@ -3,9 +3,25 @@
 var juke = angular.module('juke', ['ui.router'])
 
 juke.config(function ($stateProvider) {
-  $stateProvider.state('albumList', {
+  $stateProvider
+  .state('albumList', {
     url: '/albums',
     templateUrl: '/templates/albums.html',
     controller: 'AlbumsCtrl'
+  })
+  .state('album', {
+    url: '/albums/:id',
+    templateUrl: '/templates/album.html',
+    controller: 'AlbumCtrl'
+  })
+  .state('artistList', {
+    url: '/artists',
+    templateUrl: '/templates/artists.html',
+    controller: 'ArtistsCtrl'
+  })
+  .state('artist', {
+    url: '/artists/:id',
+    templateUrl: '/templates/artist.html',
+    controller: 'ArtistCtrl'
   });
 });
